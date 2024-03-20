aefbe341368bd97857b031a6b9b0f5a80500829b
69VR69 <44296242+69VR69@users.noreply.github.com>
2024-03-20 09:50:47 +0100

    Add noise struct and functions to apply multiple noises to the surface

--- planet.glsl
+++ planet.glsl
@@ -3,6 +3,13 @@ struct Surface {
     vec3 col; // color
 };
 
+struct Noise {
+    float factor; // factor to multiply the noise by   
+    int octaves; // number of octaves that represent how many times the noise is repeated
+    float lacunarity; // factor that determines how much the frequency of the noise increases with each octave
+    float gain; // factor that determines how much the amplitude of the noise decreases with each octave
+};
+
 Surface sdSphere(vec3 p, float s, vec3 col) {
     float d = length(p) - s;
     return Surface(d, col);
@@ -43,52 +50,76 @@ float noise(vec3 x) {
                    mix(hash(n + 270.0), hash(n + 271.0), f.x), f.y), f.z);
 }
 
-
-Surface apply_octave_noise(in Surface s, vec3 x, int octaves, float factor) {
-    float n = 0.;
-    float a = factor < 1. ? 1. - factor : 1.;
+float fbm(vec3 x, int octaves, float lacunarity, float gain) {
+    float sum = 0.0;
+    float amp = 1.0;
+    float freq = 1.0;
     for(int i = 0; i < octaves; i++) {
-        n += a * noise(x);
-        x *= 2.;
-        a *= 0.5;
+        sum += amp * noise(x * freq);
+        amp *= gain;
+        freq *= lacunarity;
     }
-    s.sd -= factor * n;
+    return sum;
+}
+
+Surface apply_noises(in Surface s, vec3 x, Noise[10] n) {
+    float accumulated_noise = 0.;
+    for(int i = 0; i < n.length(); i++) {
+        if(n[i].factor == 0.) continue;
+        accumulated_noise += n[i].factor * fbm(x, n[i].octaves, n[i].lacunarity, n[i].gain);
+    }
+    s.sd -= accumulated_noise;
     return s;
 }
+
+float getFactorSum(Noise[10] noises) {
+    float sum = 0.;
+    for(int i = 0; i < noises.length(); i++) {
+        sum += noises[i].factor;
+    }
+    return sum;
+}
  
-vec3 apply_coloring(in Surface s) {
+vec3 apply_coloring(in Surface s, in float base) {
         // Coloring depending on the distance
         const vec3 high_color = vec3(0.89, 0.42, 0.04);
-        const float high_level = 0.7;
+        float high_level = base + 0.7;
         const vec3 mid_color = vec3(0.12, 0.47, 0.04);
-        const float mid_level = 0.5;
+        float mid_level = base + 0.5;
         const vec3 low_color = vec3(0.13, 0.33, 0.88);
-        const float low_level = 0.0;
+        float low_level = base + 0.0;
 
         // Coloring
-        float d = s.sd * 1.2;
+        float d = s.sd*1.3;
 
         if (d >= high_level) {
             return high_color;
-        } else if (d >= mid_level) {
+        } else if (d >= mid_level && d < high_level) {
             return mix(mid_color, high_color, (d - mid_level) / (high_level - mid_level));
-        } else if (d >= low_level) {
+        } else if (d >= low_level && d < mid_level) {
             return mix(low_color, mid_color, (d - low_level) / (mid_level - low_level));
-        } else {
+        } else if (d < low_level){
             return low_color;
         }
     }
 
 //---------------------------------
-//includes Coloring
+
 Surface map(in vec3 pos) {
     vec3 k = pos;
-    Surface s = sdSphere(k, 0.5, vec3(0.));
+    Surface s = sdSphere(k, 0.0005, vec3(0.));
 
-    // Add some noise
-    Surface s1 = apply_octave_noise(s, k, 4, .5);
-    Surface s2 = apply_octave_noise(s1, k * 2.0, 2, .5);
-    s2.col = apply_coloring(s);
+    Noise[10] planetNoises;
+    planetNoises[0] = Noise(0.52, 6, 2.2, 0.35);
+    planetNoises[1] = Noise(0.6, 6, 1.4, 0.6);
+    planetNoises[2] = Noise(0.3, 10, 5., 0.1);
+    planetNoises[3] = Noise(0.2, 10, 9., 0.1);
+
+    Surface s2 = apply_noises(s, k, planetNoises);
+
+    float base = getFactorSum(planetNoises);
+
+    s2.col = apply_coloring(s, base);
 
     return s2;
 }
@@ -140,15 +171,18 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec3 col = vec3(0.0);
 
    // 3) Shading
+    vec3 ambiant_color = vec3(0.05, 0.1, 0.15);
+    vec3 light_color = vec3(0.97, 0.85, 0.78);
+
     if(t < 50.0) {
         vec3 pos = ro + t * rd;
         vec3 nor = calcNormal(pos);
         vec3 lig = normalize(vec3(1.0, 1., 0.2));
-        lig = rotate(lig, vec3(0.0, 1.0, 0.5), 1.5 * iTime);
+        lig = rotate(lig, vec3(1.0, 0.3, 1.), 1.5 * iTime);
         float dif = clamp(dot(nor, lig), 0.0, 1.0);
         float sha = calcSoftshadow(pos, lig, 0.001, 1.0, 16.0);
         float amb = 0.5 + 0.5 * nor.y;
-        col = vec3(0.05, 0.1, 0.15) * amb + vec3(1.00, 0.9, 0.80) * dif * sha * s.col;
+        col = vec3(0.05, 0.1, 0.15) * amb + light_color * dif * sha * s.col;
     }
 
     col = sqrt(col);
